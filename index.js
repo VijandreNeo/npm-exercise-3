e6eee9f2e7a22579e247f2d34f24c4bb7f734aa4
Neo Jan Erich Vijandre <neojanerich.vijandre@neojanerich.vijandre-CH4K3HF5WM>
2022-10-07 11:51:07 +0800

    expression parser exercise

---
+++ index.js
@@ -0,0 +1,9 @@
+const [{ parse, toString }, { argv }] = [require('@estilles/expression-parser'), require('node:process')];
+
+const [,,...args] = argv;
+
+if(args.length > 1) {
+    console.log("Error: More than one argument.");
+} else {
+    console.log(toString(parse(args[0])));
+}
